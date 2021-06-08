395245ccc43a886018db3ea85c81f43e3fd305b6
ChinmaySenke1229 <77107522+ChinmaySenke1229@users.noreply.github.com>
2021-06-08 17:19:59 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,27 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("N O I T C N F", 700,150)
+    fill("lightblue")
+    text("Hint: A specific task done by something !!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("E D T B A A S", 500,210)
+    fill("lightblue")
+    text("Hint: Firebase  !!", 500,230)
+// add code for displaying rest of the names and the hints.
+    
+
+}
